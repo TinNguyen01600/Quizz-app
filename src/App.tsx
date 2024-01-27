41310554ba732c37bfdbd878a02f5467a291595a
TinNguyen <ductin16111@gmail.com>
2024-01-27 20:14:29 +0200

    fetch data from API and add game play states

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import QuestionCard from "./components/QuestionCard";
+import { fetchQuizQuestions, Difficulty } from "./features/API";
+
+const TOTAL_QUESTIONS = 10
 
 function App() {
+	const [loading, setLoading] = useState(false);
+	const [questions, setQuestions] = useState([]);
+	const [number, setNumber] = useState(0);
+	const [userAnswer, setUserAnswer] = useState([]);
+	const [score, setScore] = useState(0);
+	const [gameOver, setGameOver] = useState(true);
+
 	const startTrivia = async () => {};
 	const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
 	const nextQuestion = () => {};
+
+    useEffect(() => {
+        console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
+        // setQuestions(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
+    })
+
 	return (
 		<>
 			<h1>REACT QUIZZ</h1>
@@ -13,7 +29,14 @@ function App() {
 			</button>
 			<p className="score">Score: </p>
 			<p>Loading Questions ...</p>
-			<QuestionCard />
+			{/* <QuestionCard
+				questionNr={number+1}
+				answers={questions[number].answers}
+				callback={checkAnswer}
+				userAnswer={userAnswer ? userAnswer[number] : undefined}
+				question={questions[number].question}
+				totalQuestions={TOTAL_QUESTIONS}
+			/> */}
 			<button className="next" onClick={nextQuestion}>
 				Next Question
 			</button>
--- src/features/API.ts
+++ src/features/API.ts
@@ -1,1 +1,28 @@
 // create logic for fetching data from the API
+
+import axios from "axios";
+
+export type Question = {
+	category: string;
+	correct_answer: string;
+	incorrect_answers: string[];
+	difficulty: string;
+	question: string;
+	type: string;
+};
+export type QuestionState = Question & { answers: string[] };
+export enum Difficulty {
+	EASY = "easy",
+	MEDIUM = "medium",
+	HARD = "hard",
+}
+export const fetchQuizQuestions = async (
+	amount: number,
+	difficulty: Difficulty
+) => {
+	const res = await axios(
+		`https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`
+	);
+	const data = res.data;
+    console.log(data)
+};
