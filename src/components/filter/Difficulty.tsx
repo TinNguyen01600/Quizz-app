e8ce4eba51d9a69ed00010b2a08e8df9a579673a
TinNguyen <ductin16111@gmail.com>
2024-02-04 23:13:53 +0200

    add state for Difficulty

--- src/components/filter/Difficulty.tsx
+++ src/components/filter/Difficulty.tsx
@@ -1,20 +1,44 @@
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
+import { Difficulty as Diff } from "../../features/API";
+import React from "react";
 
-const Difficulty = () => {
+type Props = {
+	setDifficulty: (difficulty: Diff) => void;
+};
+const Difficulty: React.FC<Props> = ({ setDifficulty }) => {
 	return (
-        <>
-        <h2>Difficulty</h2>
-        <Box>
-			<ButtonGroup variant="text" aria-label="text button group" size="large">
-				<Button sx={{fontWeight: 500, color: "purple"}}>Easy</Button>
-				<Button sx={{fontWeight: 500, color: "purple"}}>Medium</Button>
-				<Button sx={{fontWeight: 500, color: "purple"}}>Hard</Button>
-			</ButtonGroup>
-		</Box>
-        </>
-		
+		<>
+			<h2>Difficulty</h2>
+			<Box>
+				<ButtonGroup
+					variant="text"
+					aria-label="text button group"
+					size="large"
+					color="secondary"
+				>
+					<Button
+						sx={{ fontWeight: 500, color: "purple" }}
+						onClick={() => setDifficulty(Diff.EASY)}
+					>
+						Easy
+					</Button>
+					<Button
+						sx={{ fontWeight: 500, color: "purple" }}
+						onClick={() => setDifficulty(Diff.MEDIUM)}
+					>
+						Medium
+					</Button>
+					<Button
+						sx={{ fontWeight: 500, color: "purple" }}
+						onClick={() => setDifficulty(Diff.HARD)}
+					>
+						Hard
+					</Button>
+				</ButtonGroup>
+			</Box>
+		</>
 	);
 };
 
--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -3,10 +3,12 @@ import MySlider from "./MySlider";
 import MyRadioGroup from "./MyRadioGroup";
 import { Wrapper } from "../../styles/Filter.styles";
 import Difficulty from "./Difficulty";
+import { Difficulty as Diff } from "../../features/API";
 
 const Filter = () => {
 	const [totalQuestions, setTotalQuestions] = useState<number>(10);
 	const [category, setCategory] = useState<string>("art");
+    const [difficulty, setDifficulty] = useState<Diff>(Diff.EASY)
 
 	return (
 		<Wrapper>
@@ -15,7 +17,7 @@ const Filter = () => {
 				setTotalQuestions={setTotalQuestions}
 			/>
 			<MyRadioGroup category={category} setCategory={setCategory} />
-            <Difficulty />
+            <Difficulty setDifficulty={setDifficulty}/>
 		</Wrapper>
 	);
 };
